6c2813ab7af80362156aa4e2d10ac8ebb39fa249
hkaya15 <kayahuseyin15@gmail.com>
2022-02-24 01:16:05 +0300

    identifiying the variables

--- first_assignment/main.go
+++ first_assignment/main.go
@@ -2,6 +2,29 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Hello World")
+// 1) Define variables with 3 different methods
+
+// var studentName string = "John Doe"
+// var grade = 77
+
+// 2) Define variables in one line
+
+// var (
+// 	studentName string = "John Doe"
+// 	grade       int    = 77
+// 	isPassed           = true
+// )
+
+// var studentName, grade, isPassed = "John Doe",77,true
+
+func main() {
+	// 1)
+	// isPassed := true
+
+	//2)
+	studentName,grade,isPassed := "John Doe", 77 , true
+
+	fmt.Println(studentName)
+	fmt.Println(grade)
+	fmt.Println(isPassed)
 }
